953c332122a726fc79c9b40d41dc997af3628fb4
dks177 <devanshkrsingh177@gmail.com>
2025-05-15 15:04:56 +0000

    stack_heap

---
+++ .devcontainer/basics/stack_heap.js
@@ -0,0 +1,12 @@
+let name="Devansh"
+console.log(name);
+name="Harsh"
+console.log(name);
+let userone={
+    email:"devksingh",
+    id:2022,
+}
+console.log(userone.id)
+let usertwo=userone
+usertwo.id=2023
+console.log(userone.id)
